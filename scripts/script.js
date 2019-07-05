31c50d626a6b8f8fbe78dd1eea1ced6bd750fd7c
Gleb Albovsky <leb.lbovsky@gmail.com>
2019-07-05 17:31:53 +0200

    Add units, add user choise tracker

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 function convert() {
+	determineUnit();
 	var text = document.getElementById("number").value;
 	number = parseFloat(text);
+	number = number * 100;
 	document.getElementById("result").value = number;
+}
+
+function determineUnit() {
+	var index = document.getElementById("mySelect").selectedIndex;
+	alert(index);
 }
