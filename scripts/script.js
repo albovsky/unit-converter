85051fa415d13dbb48af547b07a0ff6fca235c78
Gleb Albovsky <leb.lbovsky@gmail.com>
2019-07-06 22:56:44 +0200

    Add complete conversion

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,38 @@
+var text;
+var number;
+var index;
+var index2;
+
 function convert() {
-	determineUnit();
-	var text = document.getElementById("number").value;
+	text = document.getElementById("number").value;
 	number = parseFloat(text);
-	number = number * 100;
+	determineUnit();
+	convertUnit();
 	document.getElementById("result").value = number;
 }
 
 function determineUnit() {
-	var index = document.getElementById("mySelect").selectedIndex;
-	alert(index);
+	index = document.getElementById("mySelect1").selectedIndex;
+	if (index === 0) {
+	number = number * 1000;
+	} else if (index === 1) {
+		number = number * 1;
+	} else if (index === 2) {
+		number = number * 0.01;
+	} else {
+		number = number * 0.001;
+	}
+}
+
+function convertUnit() {
+	index2 = document.getElementById("mySelect2").selectedIndex;
+	if (index2 === 0) {
+	number = number * 0.001;
+	} else if (index2 === 1) {
+		number = number * 1;
+	} else if (index2 === 2) {
+		number = number * 100;
+	} else {
+		number = number * 1000;
+	}
 }
